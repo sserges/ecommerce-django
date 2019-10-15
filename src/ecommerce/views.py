3fda0c481d1e62604280474af6aff5bbf8c6551c
sserges <agounkeserge@gmail.com>
2019-10-15 10:48:53 +0000

    HTML Form

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -52,4 +52,11 @@ def contact_page(request):
         "title": "contact Page",
         "content": "Welcome to the contact page"
     }
-    return render(request, "home_page.html", context)
+
+    if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+
+    return render(request, "contact/view.html", context)
