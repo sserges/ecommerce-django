2fc52a9495f11ee516d04b4801dea31c500e2ed8
sserges <agounkeserge@gmail.com>
2019-10-16 17:08:40 +0000

    User Login

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 
 def home_page_old(request):
@@ -67,3 +68,39 @@ def contact_page(request):
     #     print(request.POST.get('content'))
 
     return render(request, "contact/view.html", context)
+
+
+def login_page(request):
+    login_form = LoginForm(request.POST or None)
+    context = {
+        "form": login_form
+    }
+
+    print("Before authen", request.user.is_authenticated())
+
+    if login_form.is_valid():
+        print(login_form.cleaned_data)
+
+        username = login_form.cleaned_data.get("username")
+        password = login_form.cleaned_data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            print("After authen", request.user.is_authenticated())
+            login(request, user)
+            # context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            print("Error")
+
+    return render(request, "auth/login.html", context)
+
+
+def register_page(request):
+    login_form = LoginForm(request.POST or None)
+
+    if login_form.is_valid():
+        print(login_form.cleaned_data)
+
+    return render(request, "auth/login.html", {})
