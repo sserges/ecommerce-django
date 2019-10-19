c61205636c9960424166190a9c6153b22aba359c
sserges <agounkeserge@gmail.com>
2019-10-19 11:54:42 +0000

    Detail View

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 
 from products.views import (
     ProductListView,
-    product_list_view
+    product_list_view,
+    ProductDetailView,
+    product_detail_view
 )
 from .views import (
     home_page, 
@@ -38,6 +40,8 @@ urlpatterns = [
     url(r'^register/$', register_page),
     url(r'^products/$', ProductListView.as_view()),
     url(r'^products-fbv/$', product_list_view),
+    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
     url(r'^admin/', admin.site.urls),
 ]
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
@@ -20,3 +20,22 @@ def product_list_view(request):
     }
 
     return render(request, 'products/list.html', context)
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    # instance = Product.objects.get(pk=pk)
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': instance
+    }
+
+    return render(request, 'products/detail.html', context)
