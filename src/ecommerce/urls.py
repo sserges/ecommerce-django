03f1b0052f54b0c0e70aeedeeec03a33b81019be
sserges <agounkeserge@gmail.com>
2019-10-19 10:32:28 +0000

    List View

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -18,7 +18,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 
-from .views import home_page, about_page, contact_page, login_page, register_page
+from products.views import (
+    ProductListView,
+    product_list_view
+)
+from .views import (
+    home_page, 
+    about_page, 
+    contact_page, 
+    login_page, 
+    register_page
+)
 
 urlpatterns = [
     url(r'^$', home_page),
@@ -26,6 +36,8 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
+    url(r'^products/$', ProductListView.as_view()),
+    url(r'^products-fbv/$', product_list_view),
     url(r'^admin/', admin.site.urls),
 ]
 
