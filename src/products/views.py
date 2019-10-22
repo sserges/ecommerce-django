287ffaabe67f7f27d567d0311ca3024ec26dae37
sserges <agounkeserge@gmail.com>
2019-10-22 17:53:31 +0000

    Understanding Lookups

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.http import Http404
 
 from .models import Product
 
@@ -33,7 +34,22 @@ class ProductDetailView(DetailView):
 
 def product_detail_view(request, pk=None, *args, **kwargs):
     # instance = Product.objects.get(pk=pk)
-    instance = get_object_or_404(Product, pk=pk)
+    # instance = get_object_or_404(Product, pk=pk)
+
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    # except Product.DoesNotExist:
+    #     print('no product here')
+    #     raise Http404("Product doesn't exist")
+    # except:
+    #     print("huh?")
+
+    qs = Product.objects.filter(id=pk)
+    if qs.exists() and qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Product doesn't exist")
+    
     context = {
         'object': instance
     }
