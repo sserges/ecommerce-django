e4d43be13c56de4b47923c61f50fea98cffb17bb
sserges <agounkeserge@gmail.com>
2019-11-13 17:07:44 +0000

    Display the Query to the User

--- src/search/views.py
+++ src/search/views.py
@@ -5,13 +5,18 @@ from products.models import Product
 
 
 class SearchProductView(ListView):
-    template_name = "products/list.html"
+    template_name = "search/view.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        context['query'] = self.request.GET.get('q')
+        return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        print(request.GET)
-        query = request.GET.get('q', 'shirt')
-        print(query)
+        # print(request.GET)
+        query = request.GET.get('q')
+        # print(query)
         if query is not None:
             return Product.objects.filter(title__icontains=query)
-        return Product.objects.none()
+        return Product.objects.features()
